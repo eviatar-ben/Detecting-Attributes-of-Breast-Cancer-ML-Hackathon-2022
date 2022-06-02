b5c94ee7446f5345b78a1262d85448cc4933afb4
spor147 <yuvalm159@gmail.com>
2022-06-02 18:50:33 +0300

    working on part1

--- part_1_estimation.py
+++ part_1_estimation.py
@@ -1,57 +1,63 @@
+import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from explore_data import preprocessing
-from main import handle_numerical, handle_ordered_categories, handle_side
-from preprocessor import handle_categorical_cols, drop_cols
+from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import BaggingRegressor, AdaBoostRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+from sklearn.model_selection import cross_val_score, KFold
+from main import parse_features
 
-def load_data(train_X_fn):
-    features = pd.read_csv(train_X_fn, parse_dates=[
-        "אבחנה-Diagnosis date",
-        "אבחנה-Surgery date1",
-        "אבחנה-Surgery date2",
-        "אבחנה-Surgery date3",
-        "surgery before or after-Activity date"
-    ], infer_datetime_format=True, dayfirst=True)
-    return features
+
+# python3 evaluate_part_1.py --gold=./splited_datasets/y_test_base1.csv --pred=./part1_baseline_pred.csv
+def estimate_1():
+    features = pd.read_csv("splited_datasets/X_train_base1.csv")
+
+    df, num_imp, ord_imp, encoder = parse_features(features)
+
+    labels = pd.read_csv("splited_datasets/y_train_base1.csv")
+
+    model = LinearRegression()
+    model.fit(df, labels)
+
+    test_features = pd.read_csv("splited_datasets/X_test_base1.csv")
+
+    X_test, num_imp, ord_imp, encoder = parse_features(test_features, num_imp, ord_imp, encoder)
+    #
+    # pred = model.predict(X_test)
+    #
+    # pred[pred < 0] = 0
+    #
+    # pd.DataFrame(pred, columns=["אבחנה-Tumor size"]).to_csv("part1_baseline_pred.csv", index=False)
+
+    df = pd.concat([df, X_test])
+    labels = pd.concat([labels, pd.read_csv("splited_datasets/y_test_base1.csv")])
+
+    print("################## RidgeCV ##################")
+    model1 = RidgeCV()
+    print(cross_val_score(model1, df, labels, scoring="neg_mean_squared_error", cv=KFold(shuffle=True)))
+
+    print("################## LassCV ##################")
+    model2 = LassoCV()
+    print(cross_val_score(model2, df, labels.to_numpy().T[0], scoring="neg_mean_squared_error", cv=KFold(shuffle=True)))
+
+    print("################## DecisionTree ##################")
+    model3 = DecisionTreeRegressor()
+    print(cross_val_score(model3, df, labels, scoring="neg_mean_squared_error", cv=KFold(shuffle=True)))
+
+    print("################## KneighborsRegressor ##################")
+    model4 = KNeighborsRegressor()
+    print(cross_val_score(model4, df, labels, scoring="neg_mean_squared_error", cv=KFold(shuffle=True)))
+
+    print("################## average of DecisionTree and  Knn##################")
+    model5 = KNeighborsRegressor()
+    model6 = DecisionTreeRegressor()
+    print(cross_val_score(model5, df, labels, scoring="neg_mean_squared_error", cv=KFold(shuffle=True)))
 
 
 if __name__ == '__main__':
-    df = load_data("splited_datasets/X_train_base1")
-    labels = pd.read_csv("splited_datasets/y_train_base1")
-
-    handle_numerical(df)
-
-    df = handle_categorical_cols(df)
-    preprocessing(df)
-
-    handle_ordered_categories(df)
-    handle_side(df)
-
-    drop_cols(df, ['User Name',
-                   'אבחנה-Her2',
-                   'אבחנה-Ivi -Lymphovascular invasion',  # TODO
-                   'אבחנה-KI67 protein',  # TODO
-                   'אבחנה-N -lymph nodes mark (TNM)',  # TODO
-                   'אבחנה-Side',
-                   'אבחנה-Stage',  # TODO
-                   'אבחנה-Surgery name1',  # TODO
-                   'אבחנה-Surgery name2',  # TODO
-                   'אבחנה-Surgery name3',  # TODO
-                   'אבחנה-T -Tumor mark (TNM)',  # TODO
-                   'אבחנה-Tumor depth',  # TODO
-                   'אבחנה-Tumor width',  # TODO
-                   'אבחנה-er',
-                   'אבחנה-pr',
-                   'id-hushed_internalpatientid',
-                   'surgery before or after-Actual activity',  # TODO
-                   # TODO: retry dates with manual parse for Unknowns?
-                   'אבחנה-Surgery date1',
-                   'אבחנה-Surgery date2',
-                   'אבחנה-Surgery date3',
-                   'surgery before or after-Activity date',
-                   'אבחנה-Diagnosis date',
-                   ])
-
-    print(df)
-    base_model = LinearRegression()
-    base_model.fit(df, labels)
+    for i in range(5):
+        np.random.seed(i)
+        print(f"################## starting seed {i} ##################")
+        estimate_1()
