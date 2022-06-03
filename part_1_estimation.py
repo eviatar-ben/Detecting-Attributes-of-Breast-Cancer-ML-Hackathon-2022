ecc2c612fee8e7efaf0adcb6670abc7c09fb653f
spor147 <yuvalm159@gmail.com>
2022-06-03 04:05:55 +0300

    data

--- part_1_estimation.py
+++ part_1_estimation.py
@@ -6,17 +6,22 @@ from main import parse_features
 
 
 # python3 evaluate_part_1.py --gold=./splited_datasets/y_test_base1.csv --pred=./part1_baseline_pred.csv
-def estimate_1(df, labels):
-    model5 = RandomForestRegressor()
+def estimate_1(data, label):
     forset_loss = np.mean(
-        cross_val_score(model5, df, labels.to_numpy().T[0], scoring="neg_mean_squared_error", cv=KFold(shuffle=True)))
+        cross_val_score(RandomForestRegressor(max_features=0.75, max_samples=0.75), data, label.to_numpy().T[0],
+                        scoring="neg_mean_squared_error", cv=KFold(shuffle=True)))
 
     return forset_loss
 
 
 if __name__ == '__main__':
     losses = []
-    features = pd.read_csv("splited_datasets/X_train_one.csv")
+    features = pd.read_csv("splited_datasets/X_train_one.csv", parse_dates=[
+        "אבחנה-Diagnosis date",
+        "אבחנה-Surgery date1",
+        "אבחנה-Surgery date2",
+        "אבחנה-Surgery date3"
+    ], infer_datetime_format=True, dayfirst=True)
 
     df, num_imp, ord_imp, encoder = parse_features(features)
 
